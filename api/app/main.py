ad6a4cbbd9af9dacc83adca5536c47674ab1344f
Khairul <escudolinkz@gmail.com>
2025-08-23 19:57:41 +0800

    Allow CORS preflight requests

--- api/app/main.py
+++ api/app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.APP_URL],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
